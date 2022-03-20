55a5eb948a03b624b9bb336ed50f6346e1183ea8
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-03-20 13:01:20 +0100

    test(packages): configure jest in the package utils

---
+++ packages/utils/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['.'],
+  preset: 'ts-jest',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  maxWorkers: 1,
+  snapshotFormat: {
+    "printBasicPrototype": false
+  }
+};
