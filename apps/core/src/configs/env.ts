3c66a353cee2b864518c3f0a3d1158d6b89bf636
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-03-25 07:58:49 +0100

    feat(core): set up the graphql server

--- apps/core/src/configs/env.ts
+++ apps/core/src/configs/env.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
-import { AppEnvironmentVariables } from "../types/common";
+import dotenv from 'dotenv';
+import { AppEnvironmentVariables } from '../types/common';
 
 dotenv.config();
 
-const { HOST, PORT, NODE_ENV, ENABLE_INTROSPECTION } = process.env;
+const { ENABLE_INTROSPECTION, HOST, NODE_ENV, PORT } = process.env;
 
 export const env: AppEnvironmentVariables = {
   ENABLE_INTROSPECTION: ENABLE_INTROSPECTION === 'true',
@@ -11,4 +11,4 @@ export const env: AppEnvironmentVariables = {
   IS_DEV: NODE_ENV !== 'production',
   IS_PROD: NODE_ENV === 'production',
   PORT: parseInt(PORT ?? '7501', 10),
-}
+};
--- apps/core/src/index.ts
+++ apps/core/src/index.ts
@@ -2,13 +2,17 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './resources/newsletter/schema';
 import { resolvers } from './resources/newsletter/resolvers';
 import { sortNumbers } from '@sharingan/utils';
-import { env } from "./configs/env";
+import { env } from './configs/env';
 
-const server = new ApolloServer({ healthCheckPath: '/health', introspection: env.ENABLE_INTROSPECTION, resolvers, typeDefs });
+const server = new ApolloServer({
+  healthCheckPath: '/health',
+  introspection: env.ENABLE_INTROSPECTION,
+  resolvers,
+  typeDefs,
+});
 
 console.log(sortNumbers([67, 80, 4, 11, 90, 54, 22]));
 
 void server.listen({ port: env.PORT }).then(({ url }) => {
-
   console.log(`🚀  Server ready at ${url}graphql`);
 });
