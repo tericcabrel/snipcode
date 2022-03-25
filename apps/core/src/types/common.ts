58ed8944a64174c1d4a56787d68650e3472613c9
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-03-25 07:58:49 +0100

    feat(core): set up the graphql server

--- apps/core/src/types/common.ts
+++ apps/core/src/types/common.ts
@@ -1,8 +1,8 @@
-import { EnvironmentVariables } from "../../env";
+import { EnvironmentVariables } from '../../env';
 
 export type AppEnvironmentVariables = Omit<EnvironmentVariables, 'ENABLE_INTROSPECTION' | 'PORT'> & {
   ENABLE_INTROSPECTION: boolean;
-  PORT: number;
   IS_DEV: boolean;
   IS_PROD: boolean;
+  PORT: number;
 };
