7342344914577b7b1cbc90ad205ee7347c0b5a4f
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-04-01 00:03:14 +0200

    feat(database): create entities for the user and role

--- packages/database/prisma/migrations/20220331220114_create_users_and_roles_table/migration.sql
+++ packages/database/prisma/migrations/20220331220114_create_users_and_roles_table/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE `roles` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` VARCHAR(50) NOT NULL,
     `name` VARCHAR(50) NOT NULL,
     `level` INTEGER NOT NULL,
     `description` VARCHAR(200) NULL,
@@ -13,19 +13,20 @@ CREATE TABLE `roles` (
 
 -- CreateTable
 CREATE TABLE `users` (
-    `id` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
+    `id` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
     `username` VARCHAR(30) NULL,
     `first_name` VARCHAR(50) NULL,
     `last_name` VARCHAR(50) NULL,
     `password` VARCHAR(100) NULL,
     `timezone` VARCHAR(50) NULL,
-    `is_enabled` BOOLEAN NOT NULL DEFAULT true,
-    `pciture_url` VARCHAR(255) NULL,
-    `role_id` VARCHAR(191) NOT NULL,
+    `is_enabled` BOOLEAN NOT NULL DEFAULT false,
+    `picture_url` VARCHAR(255) NULL,
+    `role_id` VARCHAR(50) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_role_id_key`(`role_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ packages/database/src/entities/role.ts
@@ -0,0 +1,3 @@
+import { Role } from '@prisma/client';
+
+export default Role;
