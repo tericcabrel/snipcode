f1bd5ec5f8e641f1bbb3a5a73cf2a7c9e16a2942
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-04-10 20:25:04 +0200

    feat(core): create mutation to subscribe user to a newsletter

--- apps/core/env.d.ts
+++ apps/core/env.d.ts
@@ -1,5 +1,7 @@
 export type EnvironmentVariables = {
   ADMIN_PASSWORD: string;
+  CONVERTKIT_API_KEY: string;
+  CONVERTKIT_FORM_ID: string;
   DATABASE_URL: string;
   ENABLE_INTROSPECTION: string;
   HOST: string;
--- apps/core/src/configs/env.ts
+++ apps/core/src/configs/env.ts
@@ -6,6 +6,8 @@ dotenv.config();
 
 export const env: AppEnvironmentVariables = {
   ADMIN_PASSWORD: getEnv('ADMIN_PASSWORD'),
+  CONVERTKIT_API_KEY: getEnv('CONVERTKIT_API_KEY'),
+  CONVERTKIT_FORM_ID: getEnv('CONVERTKIT_FORM_ID'),
   DATABASE_URL: getEnv('DATABASE_URL'),
   ENABLE_INTROSPECTION: getEnv('ENABLE_INTROSPECTION') === 'true',
   HOST: getEnv('HOST'),
