9b5ab6f32310f53a2b2fbb1f8b737fcdddfd5d6b
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-04-10 20:25:04 +0200

    feat(core): create mutation to subscribe user to a newsletter

--- apps/core/src/resources/newsletter/mutations/subscribe.ts
+++ apps/core/src/resources/newsletter/mutations/subscribe.ts
@@ -1,7 +1,16 @@
+import { ApolloError } from 'apollo-server';
+import { newsletterService } from '@sharingan/domain';
 import { MutationResolvers } from '../../../types/graphql';
+import { logger } from '../../../configs/logger';
 
-export const subscribeToNewsletter: MutationResolvers['subscribeToNewsletter'] = (_parent, args) => {
-  console.log(args.email);
+export const subscribeToNewsletter: MutationResolvers['subscribeToNewsletter'] = async (_parent, args) => {
+  try {
+    await newsletterService.subscribe(args.email);
+  } catch (err) {
+    logger.error(err);
+
+    throw new ApolloError('NEWSLETTER_SUBSCRIBE_FAILED', 'Failed to subscribe to the newsletter.');
+  }
 
   return { message: 'Subscribed to the newsletter successfully' };
 };
