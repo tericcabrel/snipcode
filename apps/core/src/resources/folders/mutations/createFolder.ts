3f07183a6a66e8c3db7d0542e12943c2314ae4fa
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-05-04 23:51:52 +0200

    feat(core): prevent folder creation when there is an existing one

--- apps/core/src/resources/folders/mutations/createFolder.ts
+++ apps/core/src/resources/folders/mutations/createFolder.ts
@@ -1,6 +1,8 @@
 import { CreateFolderDto } from '@sharingan/domain';
+import { ApolloError } from 'apollo-server-express';
 import { getAuthenticatedUser } from '../../../configs/authentication';
 import { MutationResolvers } from '../../../types/graphql';
+import { FOLDER_ALREADY_EXIST_CODE, FOLDER_ALREADY_EXIST_MESSAGE } from '../../../utils/errors';
 
 export const createFolder: MutationResolvers['createFolder'] = async (_parent, args, context) => {
   const userId = getAuthenticatedUser(context);
@@ -8,6 +10,12 @@ export const createFolder: MutationResolvers['createFolder'] = async (_parent, a
   const {
     input: { name, parentId },
   } = args;
+
+  const isFolderExist = await context.db.folder.isFolderExistInParentFolder(parentId, name);
+
+  if (isFolderExist) {
+    throw new ApolloError(FOLDER_ALREADY_EXIST_MESSAGE(name), FOLDER_ALREADY_EXIST_CODE);
+  }
 
   const createFolderDto = new CreateFolderDto(userId, name, parentId);
 
