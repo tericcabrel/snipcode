49b56d7a665e55c4d33d68bd60e0decb744471b7
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-05-12 22:59:22 +0200

    feat(utils): create errors variable in the utils package

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,2 +1,7 @@
+import SharinganError from './src/errors/sharingan-error';
+
 export * as constants from './src/common/constants';
+export * as errors from './src/errors/messages';
 export * from './src/common/environment';
+
+export default SharinganError;
