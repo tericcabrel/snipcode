f802f16c3b021fcbbae336418661318b47d4c4d3
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-06-03 23:33:39 +0200

    test(domain): test the dto to create user root folder

---
+++ packages/domain/tests/services/folders/dtos/create-user-root-folder-dto.test.ts
@@ -0,0 +1,21 @@
+import { Folder } from '@sharingan/database';
+
+import { CreateUserRootFolderDto } from '../../../../index';
+
+describe('Test Create User Root Folder DTO', () => {
+  it('should create a valid folder object', () => {
+    const dto = new CreateUserRootFolderDto('dm34saxf6111113dabfed9tmm');
+
+    const folder = dto.toFolder();
+
+    expect(folder).toMatchObject<Folder>({
+      createdAt: expect.any(Date),
+      id: expect.any(String),
+      isFavorite: false,
+      name: '__dm34saxf6111113dabfed9tmm__',
+      parentId: null,
+      updatedAt: expect.any(Date),
+      userId: 'dm34saxf6111113dabfed9tmm',
+    });
+  });
+});
