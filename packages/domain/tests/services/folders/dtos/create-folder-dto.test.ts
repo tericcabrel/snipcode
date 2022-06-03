f3547ad7ec5bd11fa0e23b9948233807d58024ef
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-06-03 23:46:17 +0200

    test(domain): test the dto to create folder

--- packages/domain/tests/services/folders/dtos/create-folder-dto.test.ts
+++ packages/domain/tests/services/folders/dtos/create-folder-dto.test.ts
@@ -3,15 +3,18 @@ import { Folder } from '@sharingan/database';
 import { CreateFolderDto } from '../../../../index';
 
 describe('Test Create Folder DTO', () => {
-  it('should return a valid folder object', () => {
+  it('should create a valid folder object', () => {
+    // GIVEN
     const dto = new CreateFolderDto({
       name: 'blogs',
       parentId: 'cl23rzwe5000002czaedc8sll',
       userId: 'dm34saxf6111113dabfed9tmm',
     });
 
+    // WHEN
     const folder = dto.toFolder();
 
+    // THEN
     expect(folder).toMatchObject<Folder>({
       createdAt: expect.any(Date),
       id: expect.any(String),
@@ -21,5 +24,47 @@ describe('Test Create Folder DTO', () => {
       updatedAt: expect.any(Date),
       userId: 'dm34saxf6111113dabfed9tmm',
     });
+  });
+
+  it('should create the valid folder name', () => {
+    // GIVEN
+    const dto = new CreateFolderDto({
+      name: 'blogs',
+      parentId: 'cl23rzwe5000002czaedc8sll',
+      userId: 'dm34saxf6111113dabfed9tmm',
+    });
+    const expectedFolderName = 'blogs';
+
+    // WHEN
+    // THEN
+    expect(dto.name).toEqual(expectedFolderName);
+  });
+
+  it('should create the valid folder parent id', () => {
+    // GIVEN
+    const dto = new CreateFolderDto({
+      name: 'blogs',
+      parentId: 'cl23rzwe5000002czaedc8sll',
+      userId: 'dm34saxf6111113dabfed9tmm',
+    });
+    const expectedParentId = 'cl23rzwe5000002czaedc8sll';
+
+    // WHEN
+    // THEN
+    expect(dto.parentFolderId).toEqual(expectedParentId);
+  });
+
+  it('should create the valid folder user id', () => {
+    // GIVEN
+    const dto = new CreateFolderDto({
+      name: 'blogs',
+      parentId: 'cl23rzwe5000002czaedc8sll',
+      userId: 'dm34saxf6111113dabfed9tmm',
+    });
+    const expectedUserId = 'dm34saxf6111113dabfed9tmm';
+
+    // WHEN
+    // THEN
+    expect(dto.user).toEqual(expectedUserId);
   });
 });
