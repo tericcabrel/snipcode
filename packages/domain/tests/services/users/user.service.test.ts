ee25ca5c4242a616ba625679696c5491c5ca8739
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-06-07 21:22:53 +0200

    test(domain): test create and update user

--- packages/domain/tests/services/users/user.service.test.ts
+++ packages/domain/tests/services/users/user.service.test.ts
@@ -1,5 +1,13 @@
+import { User } from '@sharingan/database';
+
 import { roleService, userService } from '../../../index';
-import { cleanTestRoles, cleanTestUsers } from '../../setup/test-utils';
+import {
+  createTestUser,
+  createTestUserDto,
+  deleteTestUsersById,
+  findTestRole,
+  updateTestUserDto,
+} from '../../setup/test-utils';
 
 describe('Test User service', () => {
   beforeAll(async () => {
@@ -14,10 +22,89 @@ describe('Test User service', () => {
     const adminUser = await userService.findByEmail('teco@sharingan.dev');
 
     expect(adminUser).toBeDefined();
+
+    await deleteTestUsersById([adminUser?.id]);
   });
 
-  afterAll(async () => {
-    await cleanTestUsers();
-    await cleanTestRoles();
+  it('should create a user', async () => {
+    // GIVEN
+    const role = await findTestRole('user');
+    const createUserDto = await createTestUserDto(role.id);
+
+    // WHEN
+    const createdUser = await userService.create(createUserDto);
+
+    // THEN
+    expect(createdUser).toMatchObject<User>({
+      createdAt: expect.any(Date),
+      email: createUserDto.email,
+      id: createUserDto.toUser().id,
+      isEnabled: createUserDto.toUser().isEnabled,
+      name: createUserDto.toUser().name,
+      oauthProvider: createUserDto.toUser().oauthProvider,
+      pictureUrl: createUserDto.toUser().pictureUrl,
+      roleId: createUserDto.toUser().roleId,
+      timezone: createUserDto.toUser().timezone,
+      updatedAt: expect.any(Date),
+      username: createUserDto.toUser().username,
+    });
+
+    await deleteTestUsersById([createdUser.id]);
+  });
+
+  it('should create a user - validation check', async () => {
+    // GIVEN
+    const role = await findTestRole('user');
+    const createUserDto = await createTestUserDto(role.id);
+
+    createUserDto.isEnabled = true;
+
+    // WHEN
+    const createdUser = await userService.create(createUserDto);
+
+    // THEN
+    expect(createdUser).toMatchObject<User>({
+      createdAt: expect.any(Date),
+      email: createUserDto.email,
+      id: createUserDto.toUser().id,
+      isEnabled: createUserDto.toUser().isEnabled,
+      name: createUserDto.toUser().name,
+      oauthProvider: createUserDto.toUser().oauthProvider,
+      pictureUrl: createUserDto.toUser().pictureUrl,
+      roleId: createUserDto.toUser().roleId,
+      timezone: createUserDto.toUser().timezone,
+      updatedAt: expect.any(Date),
+      username: createUserDto.toUser().username,
+    });
+
+    await deleteTestUsersById([createdUser.id]);
+  });
+
+  it('should update user information', async () => {
+    // GIVEN
+    const currentUser = await createTestUser();
+
+    const role = await findTestRole('admin');
+    const updateUserDto = updateTestUserDto(role.id);
+
+    // WHEN
+    const updatedUser = await userService.update(currentUser, updateUserDto);
+
+    // THEN
+    expect(updatedUser).toMatchObject<User>({
+      createdAt: expect.any(Date),
+      email: currentUser.email,
+      id: currentUser.id,
+      isEnabled: updateUserDto.toUser(currentUser).isEnabled,
+      name: updateUserDto.toUser(currentUser).name,
+      oauthProvider: updateUserDto.toUser(currentUser).oauthProvider,
+      pictureUrl: updateUserDto.toUser(currentUser).pictureUrl,
+      roleId: updateUserDto.toUser(currentUser).roleId,
+      timezone: updateUserDto.toUser(currentUser).timezone,
+      updatedAt: expect.any(Date),
+      username: currentUser.username,
+    });
+
+    await deleteTestUsersById([currentUser.id]);
   });
 });
