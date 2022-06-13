bffd916e54bde5d4389e518359b57835d1782039
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-06-13 23:19:16 +0200

    feat(web): enhance cookie storage

--- apps/web/src/utils/apollo.ts
+++ apps/web/src/utils/apollo.ts
@@ -1,14 +1,14 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { useCookies } from 'react-cookie';
 
-import { COOKIE_NAME } from '@/utils/constants';
+import { COOKIE_NAME, IS_DEV } from '@/utils/constants';
 
 const useApolloClient = () => {
   const [cookies] = useCookies([COOKIE_NAME]);
 
   return new ApolloClient({
     cache: new InMemoryCache(),
-    connectToDevTools: true,
+    connectToDevTools: IS_DEV,
     credentials: 'include',
     headers: {
       Authorization: cookies[COOKIE_NAME],
