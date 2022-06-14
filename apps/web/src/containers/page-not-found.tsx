2e499caecc21a461f5fba2996805f2f294163dae
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-06-14 17:37:23 +0200

    feat(web): design the not found page

--- apps/web/src/containers/page-not-found.tsx
+++ apps/web/src/containers/page-not-found.tsx
@@ -4,22 +4,21 @@ import Logo from '@/components/icons/logo';
 
 const PageNotFound = () => {
   return (
-    <div className="min-h-full pt-16 pb-12 flex flex-col bg-red-500">
+    <div className="h-screen pt-16 pb-12 flex flex-col">
       <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex-shrink-0 flex justify-center">
           <a href="/" className="inline-flex">
-            <span className="sr-only">Workflow</span>
             <Logo />
           </a>
         </div>
         <div className="py-16">
           <div className="text-center">
-            <p className="text-sm font-semibold text-indigo-600 uppercase tracking-wide">404 error</p>
+            <p className="text-sm font-semibold text-blue-600 uppercase tracking-wide">404 error</p>
             <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Page not found.</h1>
             <p className="mt-2 text-base text-gray-500">Sorry, we couldn’t find the page you’re looking for.</p>
             <div className="mt-6">
               <Link href="/">
-                <a className="text-base font-medium text-indigo-600 hover:text-indigo-500">
+                <a className="text-base font-medium text-blue-600 hover:text-blue-500">
                   Go back home<span aria-hidden="true"> &rarr;</span>
                 </a>
               </Link>
@@ -29,16 +28,28 @@ const PageNotFound = () => {
       </main>
       <footer className="flex-shrink-0 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
         <nav className="flex justify-center space-x-4">
-          <Link href="/">
-            <a className="text-sm font-medium text-gray-500 hover:text-gray-600">Contact Support</a>
+          <Link href="mailto:contact@sharingan.dev">
+            <a className="text-sm font-medium text-gray-500 hover:text-gray-600">Support</a>
           </Link>
           <span className="inline-block border-l border-gray-300" aria-hidden="true" />
-          <Link href="/">
-            <a className="text-sm font-medium text-gray-500 hover:text-gray-600">Status</a>
+          <Link href="https://github.com/tericcabrel/sharingan">
+            <a
+              className="text-sm font-medium text-gray-500 hover:text-gray-600"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
           </Link>
           <span className="inline-block border-l border-gray-300" aria-hidden="true" />
-          <Link href="/">
-            <a className="text-sm font-medium text-gray-500 hover:text-gray-600">Twitter</a>
+          <Link href="https://twitter.com/sharinganapp">
+            <a
+              className="text-sm font-medium text-gray-500 hover:text-gray-600"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
           </Link>
         </nav>
       </footer>
