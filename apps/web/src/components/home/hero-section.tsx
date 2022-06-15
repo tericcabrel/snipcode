84848e08673af5af17e387f70f4cbc3342e04ec1
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-06-15 12:03:10 +0200

    chore(ci): deploy application with github actions

--- apps/web/src/components/home/hero-section.tsx
+++ apps/web/src/components/home/hero-section.tsx
@@ -35,7 +35,7 @@ const HeroSection = () => {
                   </linearGradient>
                 </defs>
               </svg>
-              <span className="relative"> snippets </span>
+              <span className="relative"> TIOGO </span>
             </span>
             all in one place
           </h1>
