8f1daf8401df360c4aec203001d7768cf5be673a
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-06-24 23:39:04 +0200

    feat(web): design the header of the landing

--- apps/web/src/components/layout/public/header.tsx
+++ apps/web/src/components/layout/public/header.tsx
@@ -1,15 +1,15 @@
+import Link from 'next/link';
 import { useState } from 'react';
 
-import GithubIcon from '@/components/icons/github';
 import LogoIcon from '@/components/icons/logo';
 
 const PublicHeader = () => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <header className="relative py-4 md:py-6">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between">
+    <header className="relative py-4">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between">
           <div className="flex-shrink-0">
             <a href="/" className="flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2">
               <LogoIcon />
@@ -17,12 +17,7 @@ const PublicHeader = () => {
           </div>
 
           <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="text-gray-900"
-              onClick={() => setIsExpanded(!isExpanded)}
-              aria-expanded={isExpanded}
-            >
+            <button type="button" className="text-gray-900" onClick={() => setIsExpanded(!isExpanded)} aria-expanded>
               {!isExpanded && (
                 <span aria-hidden="true">
                   <svg
@@ -53,38 +48,100 @@ const PublicHeader = () => {
             </button>
           </div>
 
-          <div className="hidden lg:flex lg:items-center lg:justify-center lg:space-x-10">
-            <a
-              href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-              title=""
-              className="text-base hidden font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-            >
-              Login
-            </a>
-            <a
-              href="https://github.com/tericcabrel/sharingan"
-              className="inline-flex items-center justify-center px-5 py-2 text-base font-semibold leading-7 text-gray-900 transition-all duration-200 bg-transparent border border-gray-900 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white"
-              rel="noopener noreferrer"
-              role="button"
-              target="_blank"
-            >
-              <GithubIcon /> <span className="ml-3">Star on GitHub</span>
-            </a>
-          </div>
+          <nav className="hidden lg:items-center lg:ml-16 lg:mr-auto lg:space-x-10 lg:flex">
+            <Link href="/snippets">
+              <a
+                title="Browse code snippets"
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded font-pj hover:text-opacity-50"
+              >
+                Snippets
+              </a>
+            </Link>
+
+            <Link href="/resources">
+              <a
+                title="Resources"
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded font-pj hover:text-opacity-50"
+              >
+                Resources
+              </a>
+            </Link>
+            <Link href="/blog">
+              <a
+                title="Blog"
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded font-pj hover:text-opacity-50"
+              >
+                Blog
+              </a>
+            </Link>
+          </nav>
+
+          <nav className="hidden lg:flex lg:items-center lg:justify-end lg:space-x-10">
+            <Link href="/sign-in">
+              <a
+                title="Sign in"
+                className="text-base font-medium text-gray-900 transition-all duration-200 rounded font-pj hover:text-opacity-50"
+              >
+                Sign in
+              </a>
+            </Link>
+
+            <Link href="/sign-up">
+              <a
+                title="Get started"
+                className="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj"
+              >
+                Get started
+              </a>
+            </Link>
+          </nav>
         </div>
+
         {isExpanded && (
           <nav>
             <div className="px-1 py-8">
               <div className="grid gap-y-7">
                 <a
-                  href="https://github.com/tericcabrel/sharingan"
-                  className="inline-flex items-center justify-center px-5 py-2 text-base font-semibold leading-7 text-gray-900 transition-all duration-200 bg-transparent border border-gray-900 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white"
-                  rel="noopener noreferrer"
-                  role="button"
-                  target="_blank"
+                  href="/snippets"
+                  title="Browse code snippets"
+                  className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50"
                 >
-                  Join community
+                  Snippets
                 </a>
+
+                <a
+                  href="/resources"
+                  title="Resources"
+                  className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50"
+                >
+                  Resources
+                </a>
+
+                <a
+                  href="/blog"
+                  title="Blog"
+                  className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50"
+                >
+                  Blog
+                </a>
+
+                <Link href="/sign-in">
+                  <a
+                    title="Sign in"
+                    className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50"
+                  >
+                    Sign in
+                  </a>
+                </Link>
+
+                <Link href="/sign-up">
+                  <a
+                    title="Get started"
+                    className="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600"
+                  >
+                    Get started
+                  </a>
+                </Link>
               </div>
             </div>
           </nav>
