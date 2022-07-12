fe85d093a1513e899cbb826e8b7359b4cca81117
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-07-12 22:30:40 +0200

    feat(web): organize dashboard

--- apps/web/src/containers/private/board.tsx
+++ apps/web/src/containers/private/board.tsx
@@ -6,10 +6,22 @@ const Home = () => {
   return (
     <PrivateLayout>
       <NextSeo title="Home" />
-      <main className="main">
-        <h1 className="title">Welcome to Dashboard</h1>
-        <br />
-      </main>
+      <div className="py-10">
+        <header>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">Dashboard</h1>
+          </div>
+        </header>
+        <main>
+          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            {/* Replace with your content */}
+            <div className="px-4 py-8 sm:px-0">
+              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+            </div>
+            {/* /End replace */}
+          </div>
+        </main>
+      </div>
     </PrivateLayout>
   );
 };
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,4 +3,6 @@ import Button from './src/forms/button';
 import TextInput from './src/forms/text-input';
 import Icon from './src/icons';
 
+export * from '@headlessui/react';
+
 export { Alert, Button, Icon, TextInput };
