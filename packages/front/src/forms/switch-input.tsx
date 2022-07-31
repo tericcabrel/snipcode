d19fa2794ab2ff88cb5b4707f03192fdb8eafc29
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-07-31 23:08:44 +0200

    chore(web): wrap switch input with a ref

--- packages/front/src/forms/switch-input.tsx
+++ packages/front/src/forms/switch-input.tsx
@@ -1,5 +1,5 @@
 import { Switch } from '@headlessui/react';
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 
 import { classNames } from '../utils/classnames';
 
@@ -10,41 +10,45 @@ type Props = {
   onChange: (value: boolean) => void;
 };
 
-const SwitchInput = ({ className = '', defaultValue = false, label, onChange }: Props) => {
-  const [enabled, setEnabled] = useState(defaultValue);
+const SwitchInput = forwardRef<HTMLDivElement, Props>(
+  ({ className = '', defaultValue = false, label, onChange }: Props, ref) => {
+    const [enabled, setEnabled] = useState(defaultValue);
 
-  const classes = classNames('flex items-center', className);
+    const classes = classNames('flex items-center', className);
 
-  const handleValueChange = (value: boolean) => {
-    setEnabled(value);
-    onChange(value);
-  };
+    const handleValueChange = (value: boolean) => {
+      setEnabled(value);
+      onChange(value);
+    };
 
-  return (
-    <Switch.Group as="div" className={classes}>
-      {label && (
-        <Switch.Label as="span" className="mr-3">
-          <span className="text-sm font-medium text-gray-900">{label}</span>
-        </Switch.Label>
-      )}
-      <Switch
-        checked={enabled}
-        onChange={handleValueChange}
-        className={classNames(
-          enabled ? 'bg-gray-900' : 'bg-gray-200',
-          'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500',
+    return (
+      <Switch.Group as="div" className={classes} ref={ref}>
+        {label && (
+          <Switch.Label as="span" className="mr-3">
+            <span className="text-sm font-medium text-gray-900">{label}</span>
+          </Switch.Label>
         )}
-      >
-        <span
-          aria-hidden="true"
+        <Switch
+          checked={enabled}
+          onChange={handleValueChange}
           className={classNames(
-            enabled ? 'translate-x-5' : 'translate-x-0',
-            'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+            enabled ? 'bg-gray-900' : 'bg-gray-200',
+            'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500',
           )}
-        />
-      </Switch>
-    </Switch.Group>
-  );
-};
+        >
+          <span
+            aria-hidden="true"
+            className={classNames(
+              enabled ? 'translate-x-5' : 'translate-x-0',
+              'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+            )}
+          />
+        </Switch>
+      </Switch.Group>
+    );
+  },
+);
+
+SwitchInput.displayName = 'SwitchInput';
 
 export default SwitchInput;
