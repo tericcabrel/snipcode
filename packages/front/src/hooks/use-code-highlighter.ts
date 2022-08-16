a091b28c401d74db96b3d4d61f50cb30f3f95b4c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-08-16 23:03:50 +0200

    chore(snippet): create a hook for code highlighter initialization

---
+++ packages/front/src/hooks/use-code-highlighter.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { Highlighter } from 'shiki';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const shiki = require('shiki');
+
+export const useCodeHighlighter = () => {
+  const [highlighter, setHighlighter] = useState<Highlighter | undefined>();
+
+  useEffect(() => {
+    const loadShikiAssets = async () => {
+      shiki.setCDN('/assets/shiki/');
+
+      return shiki.getHighlighter({
+        langs: ['javascript', 'html', 'css', 'typescript', 'java', 'c', 'cpp', 'c#', 'php', 'python'],
+        theme: 'monokai',
+        themes: ['one-dark-pro', 'dracula', 'dark-plus', 'monokai', 'github-dark', 'github-light'],
+      });
+    };
+
+    void loadShikiAssets().then((result) => {
+      setHighlighter(result);
+    });
+  }, []);
+
+  return { highlighter };
+};
