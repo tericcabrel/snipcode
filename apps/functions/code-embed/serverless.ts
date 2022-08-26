bb6ba85b542e087c14f9cdc1b54d56525342776c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-08-26 18:21:26 +0200

    chore(embed): setup lambda function for snippet renderer

---
+++ apps/functions/code-embed/serverless.ts
@@ -0,0 +1,38 @@
+import type { AWS } from '@serverless/typescript';
+
+import renderer from '@/functions/renderer';
+
+const serverlessConfiguration: AWS = {
+  custom: {
+    esbuild: {
+      bundle: true,
+      concurrency: 10,
+      define: { 'require.resolve': undefined },
+      exclude: ['aws-sdk'],
+      minify: false,
+      platform: 'node',
+      sourcemap: true,
+      target: 'node16',
+    },
+  },
+  frameworkVersion: '3',
+  // import the function via paths
+  functions: { renderer },
+  package: { individually: true },
+  plugins: ['serverless-esbuild'],
+  provider: {
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    name: 'aws',
+    runtime: 'nodejs16.x',
+  },
+  service: 'code-embed',
+};
+
+module.exports = serverlessConfiguration;
