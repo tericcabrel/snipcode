d9c718bc3ffaeb020bcbce2105f05b54fb31d9dc
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2024-05-22 21:17:19 +0200

    chore(backend): configure environment variables

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import appConfig, { validate } from './configs/environment';
 
 @Module({
   controllers: [AppController],
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.test'],
+      isGlobal: true,
+      load: [appConfig],
+      validate,
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,23 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { EnvironmentVariables } from './types/common';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService<EnvironmentVariables, true>);
+  const logger = new Logger('NestApplication');
+
+  const port = configService.get<number>('PORT');
+  const host = configService.get<string>('HOST');
+
+  await app.listen(port, () => {
+    logger.log(`Server ready at ${host}:${port}`);
+    // logger.log(`Server ready at ${host}:${port}${graphqlServer.graphqlPath}`);
+  });
 };
 
 void bootstrap();
