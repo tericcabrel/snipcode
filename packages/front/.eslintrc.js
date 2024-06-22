5bee87a95cf0fea744450ceb4b941459e1100934
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2024-06-23 01:41:50 +0200

    fix: ci

--- packages/front/.eslintrc.js
+++ packages/front/.eslintrc.js
@@ -1,15 +1,17 @@
-{
+module.exports = {
   "root": true,
   "extends": "../../.eslintrc.json",
   "ignorePatterns": [
     "postcss.config.js",
     "tailwind.config.js",
     "jest.config.ts",
-    "generated.ts"
+    "generated.ts",
+    '.eslintrc.js'
   ],
   "parserOptions": {
     "ecmaVersion": 2023,
     "sourceType": "module",
-    "project": "tsconfig.json"
+    "project": "tsconfig.json",
+    tsconfigRootDir: __dirname,
   }
-}
+};
