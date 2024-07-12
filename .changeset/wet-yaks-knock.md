1d025df57743a2ee244817a90bf1302f47535691
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2024-07-12 12:07:59 +0200

    fix(backend): catch prisma error globally

---
+++ .changeset/wet-yaks-knock.md
@@ -0,0 +1,5 @@
+---
+'@snipcode/backend': patch
+---
+
+Catch Prisma technical error and return user friendly message
--- apps/backend/src/configs/exception.filter.ts
+++ apps/backend/src/configs/exception.filter.ts
@@ -1,9 +1,35 @@
 import { ArgumentsHost, Catch } from '@nestjs/common';
 import { AbstractHttpAdapter, BaseExceptionFilter } from '@nestjs/core';
 import { GqlArgumentsHost, GqlContextType } from '@nestjs/graphql';
+import {
+  PrismaClientInitializationError,
+  PrismaClientKnownRequestError,
+  PrismaClientRustPanicError,
+  PrismaClientUnknownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
 import { isAppError } from '@snipcode/utils';
 import { Response } from 'express';
 import { GraphQLError } from 'graphql';
+
+import { INTERNAL_SERVER_ERROR } from '../utils/constants';
+
+type PrismaError =
+  | PrismaClientInitializationError
+  | PrismaClientKnownRequestError
+  | PrismaClientRustPanicError
+  | PrismaClientUnknownRequestError
+  | PrismaClientValidationError;
+
+const isPrismaError = (error: unknown): error is PrismaError => {
+  return (
+    error instanceof PrismaClientInitializationError ||
+    error instanceof PrismaClientKnownRequestError ||
+    error instanceof PrismaClientRustPanicError ||
+    error instanceof PrismaClientUnknownRequestError ||
+    error instanceof PrismaClientValidationError
+  );
+};
 
 @Catch()
 export class ApplicationExceptionFilter extends BaseExceptionFilter {
@@ -22,18 +48,31 @@ export class ApplicationExceptionFilter extends BaseExceptionFilter {
           },
           originalError: exception,
         });
+      } else if (isPrismaError(exception)) {
+        throw new GraphQLError(INTERNAL_SERVER_ERROR, {
+          extensions: {
+            code: 'DATABASE_ERROR',
+          },
+        });
       }
     } else {
-      if (isAppError(exception)) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
 
+      if (isAppError(exception)) {
         response.status(400).json({
           code: exception.code,
           message: exception.message,
           timestamp: new Date().toISOString(),
         });
+      } else if (isPrismaError(exception)) {
+        response.status(500).json({
+          code: 'DATABASE_ERROR',
+          message: INTERNAL_SERVER_ERROR,
+          timestamp: new Date().toISOString(),
+        });
       }
+
       super.catch(exception, host);
     }
   }
