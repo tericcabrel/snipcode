5b50a358646de5b390c3a49c64437b1478eb6391
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2024-09-25 22:08:51 +0200

    feat: design the header

--- apps/web/src/app/(protected)/layout/content.tsx
+++ apps/web/src/app/(protected)/layout/content.tsx
@@ -3,10 +3,9 @@
 import { useAuthenticatedUser } from '@snipcode/front/services';
 import { ReactNode } from 'react';
 
+import { Header } from '@/app/(protected)/layout/header';
 import { Loader } from '@/components/common/loader';
 import { Redirect } from '@/components/common/redirect';
-
-import { Header } from './header';
 
 type Props = {
   children?: ReactNode;
@@ -28,7 +27,7 @@ export const AuthenticatedLayout = ({ children }: Props) => {
   }
 
   return (
-    <div className="relative w-full min-h-screen bg-gray-50 flex-grow">
+    <div className="relative w-full min-h-screen bg-white flex-grow">
       <Header />
       {children}
     </div>
--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const generateInitials = (name?: string | null) => {
+  if (!name) {
+    return 'JD';
+  }
+
+  const nameArray = name.trim().split(' ');
+
+  const [firstName, secondName] = nameArray;
+
+  const initials = [firstName, secondName]
+    .filter(Boolean)
+    .map((name) => name.charAt(0))
+    .join('');
+
+  return initials.toUpperCase();
+};
